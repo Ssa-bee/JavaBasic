0f72147b6ee81560323e97bdd07fa6c813146194
yigwon <fkkje12@naver.com>
2022-04-06 14:07:34 +0900

    PolyArgument update

--- src/ch7/polyArgument/PolyArgumentTest.java
+++ src/ch7/polyArgument/PolyArgumentTest.java
@@ -6,9 +6,11 @@ public class PolyArgumentTest {
 
         b.buy(new Tv());
         b.buy(new Computer());
+        b.buy(new Audio());
 
         System.out.println(b.money);
         System.out.println(b.bonusPoint);
+        b.summary();
     }
 }
 
@@ -19,6 +21,9 @@ class Product {
     Product(int price) {
         this.price = price;
         bonusPoint = (int) (price / 10.0);
+    }
+
+    Product() {
     }
 }
 
@@ -42,17 +47,42 @@ class Computer extends Product {
     }
 }
 
-class Buyer{
+class Audio extends Product {
+    Audio() {
+        super(50);
+    }
+
+    public String toString() {
+        return "Audio";
+    }
+}
+
+class Buyer {
     int money = 1000;
     int bonusPoint = 0;
+    Product[] item = new Product[10];
+    int i = 0;
 
-    void buy(Product p){
-        if (money < p.price){
+    void buy(Product p) {
+        if (money < p.price) {
             System.out.println("잔액이 부족하여 물건을 살 수 없습니다.");
             return;
         }
         money -= p.price;
         bonusPoint += p.bonusPoint;
-        System.out.println(p +  "을/를 구입하셨습니다.");
+        item[i++] = p;
+        System.out.println(p + "을/를 구입하셨습니다.");
+    }
+    void summary(){
+        int sum = 0;
+        String itemList = "";
+
+        for (int i = 0; i < item.length; i++) {
+            if(item[i] == null) break;
+            sum += item[i].price;
+            itemList += item[i] + ", ";
+        }
+        System.out.println("sum = " + sum);
+        System.out.println("itemList = " + itemList);
     }
 }
