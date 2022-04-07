632755c7d8015032c9921365ece12a74e45b3dbd
yigwon <fkkje12@naver.com>
2022-04-07 10:17:43 +0900

    Parser Test commit

--- src/ch7/parser/ParserTest.java
+++ src/ch7/parser/ParserTest.java
@@ -4,6 +4,8 @@ public class ParserTest {
     public static void main(String[] args) {
         Parseable parser = ParserManager.getParser("XML");
         parser.parse("document.xml");
+        parser = ParserManager.getParser("HTML");
+        parser.parse("document2.html");
     }
 }
 
