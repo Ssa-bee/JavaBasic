746afb62fcea86f3a6055684b291059b4e3fd9e3
yigwon <fkkje12@naver.com>
2022-04-18 13:41:59 +0900

    Queue Test

---
+++ src/ch11/StackQueue/PriorityQueueEx.java
@@ -0,0 +1,10 @@
+package ch11.StackQueue;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueEx {
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        
+    }
+}
