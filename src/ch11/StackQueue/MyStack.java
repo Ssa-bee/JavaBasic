d487f4115dd5289488dbeb231ad6dc09e6713b47
yigwon <fkkje12@naver.com>
2022-04-18 12:38:02 +0900

    Stack Test

---
+++ src/ch11/StackQueue/MyStack.java
@@ -0,0 +1,36 @@
+package ch11.StackQueue;
+
+import java.util.*;
+public class MyStack extends Vector{
+    public Object push(Object item){
+        addElement(item);
+        return item;
+    }
+
+    public Object pop(){
+        Object obj = peek();
+        removeElement(size() - 1);
+        return obj;
+    }
+
+    public Object peek(){
+        int len = size();
+        if(len == 0){
+            throw new EmptyStackException();
+        }
+        return elementAt(len - 1);
+    }
+
+    public boolean empty(){
+        return size() == 0;
+    }
+
+    public int search(Object o){
+        int i = lastIndexOf(0);
+
+        if(i >= 0){
+            return size();
+        }
+        return - 1;
+    }
+}
