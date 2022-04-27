cc6f7c6edd5b9b62fab84fe53da56db21f1b61a9
yigwon <fkkje12@naver.com>
2022-04-27 13:22:25 +0900

    Study

--- src/ch8/HelloWorldAnonymousClasses.java
+++ src/ch8/HelloWorldAnonymousClasses.java
@@ -1,4 +1,60 @@
 package ch8;
 
 public class HelloWorldAnonymousClasses {
+    interface HelloWorld{
+        void greet();
+        void greetSomeone(String someone);
+    }
+
+    public void sayHello(){
+        class EnglishGreeting implements HelloWorld{
+            String name = "World";
+            @Override
+            public void greet() {
+                greetSomeone("world");
+            }
+
+            @Override
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hello " + name);
+            }
+        }
+
+        HelloWorld englishGreeting = new EnglishGreeting();
+
+        HelloWorld frenchGreeting = new HelloWorld() {
+            String name = "tout le monde";
+            public void greet() {
+                greetSomeone("tout le monde");
+            }
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Salut " + name);
+            }
+        };
+
+        HelloWorld spanishGreeting = new HelloWorld() {
+            String name = "mundo";
+            @Override
+            public void greet() {
+                greetSomeone("mundo");
+            }
+
+            @Override
+            public void greetSomeone(String someone) {
+                name = someone;
+                System.out.println("Hola, " + name);
+            }
+        };
+
+        englishGreeting.greet();
+        frenchGreeting.greetSomeone("Fred");
+        spanishGreeting.greet();
+    }
+
+    public static void main(String... args) {
+        HelloWorldAnonymousClasses myApp = new HelloWorldAnonymousClasses();
+        myApp.sayHello();
+    }
 }
