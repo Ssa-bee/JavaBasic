6839450240e81bace71ca798020426d60b4532ac
yigwon <yigwon@jlkgroup.com>
2022-05-12 08:34:47 +0900

    HashSet Test

--- src/ch11/hashSet/HashSet1.java
+++ src/ch11/hashSet/HashSet1.java
@@ -1,7 +1,16 @@
 package ch11.hashSet;
 
+import java.util.*;
+
 public class HashSet1 {
     public static void main(String[] args) {
-        
+        Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", "4", "4", "4"};
+        Set set = new HashSet();
+
+        for (int i = 0; i < objArr.length; i++) {
+            set.add(objArr[i]);
+        }
+
+        System.out.println(set);
     }
 }
--- src/ch11/hashSet/HashSetLotto.java
+++ src/ch11/hashSet/HashSetLotto.java
@@ -1,5 +1,18 @@
 package ch11.hashSet;
 
+import java.util.*;
+
 public class HashSetLotto {
-    
+    public static void main(String[] args) {
+        Set set = new HashSet();
+
+        for (int i = 0; set.size() < 6; i++) {
+            int num = (int) (Math.random() * 45) + 1;
+            set.add(new Integer(num));
+        }
+
+        List list = new LinkedList(set);
+        Collections.sort(list);
+        System.out.println(list);
+    }
 }
