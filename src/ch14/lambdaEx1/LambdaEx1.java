a8583c491dc3f2c7f330be5942e045edf8c64b0a
yigwon <yigwon@jlkgroup.com>
2022-05-17 16:17:25 +0900

    Lambda Test

--- src/ch14/lambdaEx1/LambdaEx1.java
+++ src/ch14/lambdaEx1/LambdaEx1.java
@@ -1,7 +1,36 @@
 package ch14.lambdaEx1;
 
 public class LambdaEx1 {
-    public static void main(String[] args) {
-        
+    static void execute(MyFunction f){
+        f.run();
     }
+
+    static MyFunction getMyFunction(){
+        MyFunction f = () -> System.out.println("f3.run()");
+        return f;
+    }
+    public static void main(String[] args) {
+        MyFunction f1 = () -> System.out.println("f1.run()");
+
+        MyFunction f2 = new MyFunction() {
+            @Override
+            public void run() {
+                System.out.println("f2.fun()");
+            }
+        };
+
+        MyFunction f3 = getMyFunction();
+
+        f1.run();
+        f2.run();
+        f3.run();
+
+        execute(f1);
+        execute(() -> System.out.println("run()"));
+    }
+}
+
+@FunctionalInterface
+interface MyFunction{
+    void run();
 }
