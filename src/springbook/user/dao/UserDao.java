413cbeb7803b66f3a520b962cf86eb7b2c604c62
Ssa-bee <fkkje12@naver.com>
2022-06-25 18:25:03 +0900

    spring framework tutorial

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -4,9 +4,19 @@ import springbook.user.domain.User;
 
 import java.sql.*;
 
-public class    UserDao {
+public class UserDao {
+    private ConnectionMaker connectionMaker;
+
+//    public UserDao(){
+//        connectionMaker = new NConnectionMaker();
+//    }
+
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) values(?, ?, ?)");
 
         ps.setString(1, user.getId());
@@ -20,7 +30,7 @@ public class    UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 
         ps.setString(1, id);
@@ -37,12 +47,5 @@ public class    UserDao {
         c.close();
 
         return user;
-    }
-
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/demo?characterEncoding=UTF-8&serverTimezone=UTC", "root", "root");
-        return c;
     }
 }
